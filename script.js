0407b742aa3fde34a945cb3419620e0a7f4303b0
monopoly1015 <123380452+monopoly1015@users.noreply.github.com>
2026-05-11 17:07:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Slideshow configuration
     const slides = [
         { type: 'video', src: 'promo.mp4', alt: 'Beer Pouring...' },
-        { type: 'image', src: 'photo3.jpg', alt: 'Beer Pouring...' },
-        { type: 'image', src: 'bar-in-use.jpg', alt: 'Beer Pouring...' },
-        { type: 'image', src: 'bar-in-use2.JPG', alt: 'BevBar' }
+        { type: 'video', src: 'anniversary.mp4', alt: 'Beer Pouring...' },
+        { type: 'image', src: 'bevbarWineSideView.jpg', alt: 'Beer Pouring...' },
+        { type: 'image', src: 'anniversary.jpg', alt: 'Beer Pouring' },
+        { type: 'image', src: 'bevbarMacParkCrowd.jpg', alt: 'Beer Pouring' }
     ];
 
     // DOM elements
